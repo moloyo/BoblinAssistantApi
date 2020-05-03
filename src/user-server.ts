e20da406211dc5ce2ecf85c0db2a8915dd2a59ff
Joaquín García <isi.garciaj@gmail.com>
2020-05-02 21:41:09 -0300

    Send connected users

--- src/user-server.ts
+++ src/user-server.ts
@@ -3,13 +3,15 @@ import { createServer, Server } from 'http';
 import * as express from 'express';
 import * as socketIo from 'socket.io';
 import { Message } from './model/message.model';
+import { User } from './model/user.model';
 
 export class UserServer {
-    public static readonly PORT:number = 8080;
+    public static readonly PORT: number = 8080;
     private app: express.Application;
     private server: Server;
     private io: SocketIO.Server;
     private port: string | number;
+    private users: { [socketId: string]: User } = {};
 
     constructor() {
         this.createApp();
@@ -42,12 +44,20 @@ export class UserServer {
 
         this.io.on('connect', (socket: any) => {
             console.log('Connected client on port %s.', this.port);
+            socket.on('user', (u: User) => {
+                console.log('[server](new user): %s', JSON.stringify(u));
+                this.users[socket.id] = u;
+                this.io.emit('users', Object.keys(this.users).map(k => this.users[k]));
+            });
+
             socket.on('message', (m: Message) => {
                 console.log('[server](message): %s', JSON.stringify(m));
                 this.io.emit('message', m);
             });
 
             socket.on('disconnect', () => {
+                delete this.users[socket.id];
+                this.io.emit('users', Object.keys(this.users).map(k => this.users[k]));
                 console.log('Client disconnected');
             });
         });
