4e6067b3fc7de7236cf31a4e91e363d3798dcf1d
Slakkan <lnpastinante@gmail.com>
2020-05-12 21:15:16 -0300

    added roll event to socket

--- src/user-server.ts
+++ src/user-server.ts
@@ -2,8 +2,9 @@
 import { createServer, Server } from 'http';
 import * as express from 'express';
 import * as socketIo from 'socket.io';
-import { Message } from './model/message.model';
-import { User } from './model/user.model';
+import { Message } from './model/chat.model';
+import { User } from './model/users.model';
+import { Roll } from './model/dice-table.model';
 
 export class UserServer {
     public static readonly PORT: number = 8080;
@@ -42,17 +43,12 @@ export class UserServer {
             console.log('Running server on port %s', this.port);
         });
 
-        this.io.on('connect', (socket: any) => {
+        this.io.on('connect', (socket: socketIo.Socket) => {
             console.log('Connected client on port %s.', this.port);
-            socket.on('user', (u: User) => {
-                console.log('[server](new user): %s', JSON.stringify(u));
-                this.users[socket.id] = u;
-                this.io.emit('users', Object.keys(this.users).map(k => this.users[k]));
-            });
-
-            socket.on('message', (m: Message) => {
-                console.log('[server](message): %s', JSON.stringify(m));
-                this.io.emit('message', m);
+            socket.on('user', (res: User) => {
+                console.log('[server](new user): %s', JSON.stringify(res));
+                this.users[socket.id] = res;
+                socket.emit('users', Object.keys(this.users).map(k => this.users[k]));
             });
 
             socket.on('disconnect', () => {
@@ -60,6 +56,16 @@ export class UserServer {
                 this.io.emit('users', Object.keys(this.users).map(k => this.users[k]));
                 console.log('Client disconnected');
             });
+
+            socket.on('message', (res: Message) => {
+                console.log('[server](message): %s', JSON.stringify(res));
+                socket.emit('message', res);
+            });
+
+            socket.on('roll', (res: Roll[]) => {
+                console.log('Roll data sent '+ res.length + ' entries')
+                socket.broadcast.emit('roll', res)
+            })
         });
     }
 
